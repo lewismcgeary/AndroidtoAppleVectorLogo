86a4f453a65093ee548487c4be098b11c1ee66e5
Lewis McGeary <lewismcgeary@gmail.com>
2016-01-20 17:15:39 +0000

    remove unnecessary code and buttons

--- app/src/main/java/com/mcgearybros/androidtoapplevectorlogo/MainActivity.java
+++ app/src/main/java/com/mcgearybros/androidtoapplevectorlogo/MainActivity.java
@@ -21,47 +21,14 @@ public class MainActivity extends Activity {
         animatorImageView = (ImageView) findViewById(R.id.path_morph_animated_vector);
         mightyMorphinAnimatedVectorDrawable = (AnimatedVectorDrawable) getDrawable(R.drawable.consolidated_animated_vector);
         mightyMorphinAnimatedVectorDrawableReversed = (AnimatedVectorDrawable) getDrawable(R.drawable.consolidated_animated_vector_reverse);
-        animatorImageView.setImageDrawable(getDrawable(R.drawable.android_logo_vector_morphable));
-
         animatorImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 morph();
             }
         });
-
-        final ImageView toAppleButton = (ImageView) findViewById(R.id.to_apple_button);
-        final ImageView toAndroidButton = (ImageView) findViewById(R.id.to_android_button);
-        toAndroidButton.setEnabled(false);
-        toAppleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toAndroidButton.setEnabled(true);
-                toAppleButton.setEnabled(false);
-                animateToApple();
-            }
-        });
-        toAndroidButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toAppleButton.setEnabled(true);
-                toAndroidButton.setEnabled(false);
-                animateToAndroid();
-            }
-        });
     }
 
-    public void animateToApple(){
-        mightyMorphinAnimatedVectorDrawable.stop();
-        animatorImageView.setImageDrawable(mightyMorphinAnimatedVectorDrawable);
-        mightyMorphinAnimatedVectorDrawable.start();
-    }
-
-    public void animateToAndroid(){
-        mightyMorphinAnimatedVectorDrawableReversed.stop();
-        animatorImageView.setImageDrawable(mightyMorphinAnimatedVectorDrawableReversed);
-        mightyMorphinAnimatedVectorDrawableReversed.start();
-    }
     public void morph(){
         AnimatedVectorDrawable currentDrawable = isShowingAndroid ? mightyMorphinAnimatedVectorDrawable : mightyMorphinAnimatedVectorDrawableReversed;
         animatorImageView.setImageDrawable(currentDrawable);
